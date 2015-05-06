31d0ec60292135c81726d160cd4c0e7cc6c8b462
Kevin_Zheng <zhengzhenyu@huawei.com>
2015-05-06 12:17:26 +0800

    delete _build

--- _build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:46,filenames:["README","index"],objects:{},objnames:{},objtypes:{},terms:{content:1,index:1,modul:1,page:1,search:1},titles:["&lt;no title&gt;","Welcome to terracotta_doc&#8217;s documentation!"],titleterms:{document:1,indic:1,tabl:1,terracotta_doc:1,welcom:1}})
